9bef240df2451cc2955a25d8688761c0a7787bb0
胡思源 <ktboyyy@outlook.com>
2018-03-08 00:59:09 -0500

    comment code

--- homework2/HW2Handout/cartpole.py
+++ homework2/HW2Handout/cartpole.py
@@ -7,8 +7,8 @@ FINAL_EPSILON = 0.05
 DECAY_ITERATION = 100000
 MAX_ITERATION_PER_EPISODE = 10000
 CONSECUTIVE_FRAMES = 4
-ENVIRONMENT_NAME = 'MountainCar-v0'
-MODEL = 'CNN'
+ENVIRONMENT_NAME = 'CartPole-v0'
+MODEL = 'MLP'
 
 class QNetwork():
 
@@ -16,14 +16,16 @@ class QNetwork():
 	# The network should take in state of the world as an input, 
 	# and output Q values of the actions available to the agent as the output. 
 
-	def __init__(self, environment_name, dueling = False, model = None):
-		# Define your network architecture here. It is also a good idea to define any training operations 
-		# and optimizers here, initialize your variables, or alternately compile your model here.
+	def __init__(self, environment_name, dueling = True, model = None):
 		env = gym.make(environment_name)
 		self.state_dim = list(env.observation_space.shape)
+
+		# The shape of the origin state could have multiple dimensions.
+		# We flat the state dimensions here
 		self.flat_state_dim = 1
 		for i in self.state_dim:
 			self.flat_state_dim *= i
+
 		self.action_dim = env.action_space.n
 		self.learning_rate = 0.0001
 		self.dueling = dueling
@@ -33,25 +35,30 @@ class QNetwork():
 		if model != None:
 			self.load_model(model)
 		else:
+			# The dropout probability
 			self.keep_prob = tf.placeholder(tf.float32, name = "keep_prob")
-			self.CreateLinearNetwork()
+
+			self.CreateMLP()
 			self.CreateOptimizer()
 
 	def CreateWeights(self, shape):
-		initial = tf.random_uniform(shape)
+		initial = tf.truncated_normal(shape, stddev = 0.1)
 		return tf.Variable(initial)
 
 	def CreateBias(self, shape):
-		initial = tf.random_uniform(shape)
+		initial = tf.constant(0.1, shape = shape)
 		return tf.Variable(initial)
 
 	def CreateConv2d(self, x, w):
+		# Create convolutional layer
 		return tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding='SAME')
 
 	def CreateMaxPool(self, x):
+		# Create 2x2 max pool layer
 		return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 	def CreateDuelingLayer(self, last_layer, unit_num):
+		# Create dueling layer under last_layer and connect to output layer
 		w_v = self.CreateWeights([unit_num, 1])
 		b_v = self.CreateBias([1])
 		v_layer = tf.add(tf.matmul(last_layer, w_v), b_v)
@@ -63,43 +70,52 @@ class QNetwork():
 		self.q_values = tf.add(v_layer, a_layer - tf.reduce_mean(a_layer, axis = 1, keepdims = True), name = "q_values")
 
 	def ConvertImage(self, images):
+		# Grayscale and resize to 84x84
 		gray_scale = tf.image.rgb_to_grayscale(images)
 		return tf.image.resize_images(gray_scale, [84, 84])
 
 	def PreprocessState(self, origin_state):
+		# Preprocess to stack four consecutive frames into one state
 		frames = []
 		for i in range(CONSECUTIVE_FRAMES):
+			# Convert each frame
 			frames.append(self.ConvertImage(origin_state[:,i,:,:,:]))
-		# Hard code here to concatenate all four frame
+
+		# Conbine four frames
 		return tf.concat([frames[0], frames[1], frames[2], frames[3]], axis = 3)
 
 	def CreateCNN(self):
+		# Create convolutional neuro network
 		# There are four consecutive frames per state
 		self.state_input = tf.placeholder(tf.float32, [None, CONSECUTIVE_FRAMES] + self.state_dim, 
 			name = "state_input")
 		converted_state = self.PreprocessState(self.state_input) # to 84*84*4
 
+		# -------- first conv + pool ---------------------------
 		w_conv1 = self.CreateWeights([8, 8, CONSECUTIVE_FRAMES, 16])
 		b_conv1 = self.CreateBias([16])
 
 		h_conv1 = tf.nn.relu(self.CreateConv2d(converted_state, w_conv1) + b_conv1)
 		h_pool1 = self.CreateMaxPool(h_conv1) # to 42*42*32
 
+		# -------- second conv + pool ---------------------------
 		W_conv2 = self.CreateWeights([4, 4, 16, 32])
 		b_conv2 = self.CreateBias([32])
 
 		h_conv2 = tf.nn.relu(self.CreateConv2d(h_pool1, W_conv2) + b_conv2)
 		h_pool2 = self.CreateMaxPool(h_conv2) # to 21*21*64
 
+		# -------- full connection ------------------------------
 		W_fc1 = self.CreateWeights([21 * 21 * 32, 256])
 		b_fc1 = self.CreateBias([256])
 
 		h_pool2_flat = tf.reshape(h_pool2, [-1, 21 * 21 * 32])
 		h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-		# self.keep_prob = tf.placeholder("float", name = "keep_prob")
+		# -------- drop out -------------------------------------
 		h_fc1_drop = tf.nn.dropout(h_fc1, self.keep_prob)
 
+		# -------- output (+ dueling)----------------------------
 		if self.dueling:
 			self.CreateDuelingLayer(h_fc1_drop, 256)
 		else:
@@ -108,6 +124,7 @@ class QNetwork():
 			self.q_values = tf.add(tf.matmul(h_fc1_drop, W_fc2), b_fc2, name = "q_values")
 
 	def CreateMLP(self):
+		# Craete multilayer perceptron (one hidden layer with 20 units)
 		self.hidden_units = 20
 
 		self.w1 = self.CreateWeights([self.flat_state_dim, self.hidden_units])
@@ -127,6 +144,7 @@ class QNetwork():
 			self.q_values = tf.add(tf.matmul(h_layer, self.w2), self.b2, name = "q_values")
 
 	def CreateLinearNetwork(self):
+		# Create linear network, output is Q value to each action
 		self.state_input = tf.placeholder(tf.float32, [None] + self.state_dim, name = "state_input")
 
 		flat_state = tf.reshape(self.state_input, [-1, self.flat_state_dim])
@@ -139,6 +157,7 @@ class QNetwork():
 			self.q_values = tf.add(tf.matmul(flat_state, w), b, name = "q_values")
 
 	def CreateOptimizer(self):
+		# Using Adam to minimize the error between target and evaluation
 		self.action_input = tf.placeholder(tf.float32, [None, self.action_dim], name = "action_input")
 		self.target_q_value = tf.placeholder(tf.float32, [None], name = "target_q_value")
 		q_value_output = tf.reduce_sum(tf.multiply(self.q_values, self.action_input), 1)
@@ -148,14 +167,14 @@ class QNetwork():
 		self.session.run(tf.global_variables_initializer())
 
 	def get_q_values(self, state):
+		# Get Q values by feeding state
 		return self.q_values.eval(feed_dict = {self.state_input : state, self.keep_prob : 1.0})
 
-	def save_model_weights(self, suffix, step):
-		# Helper function to save your model / weights.
+	def save_model(self, suffix, step):
+		# Helper function to save your model.
 		saver = tf.train.Saver()
 		tf.add_to_collection("optimizer", self.optimizer)
 		saver.save(self.session, suffix, global_step = step)
-		pass
 
 	def load_model(self, model_file):
 		# Helper function to load an existing model.
@@ -169,17 +188,9 @@ class QNetwork():
 		self.target_q_value = graph.get_tensor_by_name("target_q_value:0")
 		self.keep_prob = graph.get_tensor_by_name("keep_prob:0")
 		self.optimizer = tf.get_collection("optimizer")[0]
-		pass
-
-	def load_model_weights(self,weight_file):
-		# Helper funciton to load model weights. 
-		file = open("./test", "r")
-		print file.readline()
-		pass
 
 class Replay_Memory():
 
-	# burn_in = 10000
 	def __init__(self, memory_size=50000, burn_in=10000):
 
 		# The memory essentially stores transitions recorder from the agent
@@ -191,11 +202,9 @@ class Replay_Memory():
 		self.buffer = deque()
 		self.memory_size = memory_size
 		self.burn_in = burn_in
-		pass
 
 	def sample_batch(self, batch_size = 32):
 		# This function returns a batch of randomly sampled transitions - i.e. state, action, reward, next state, terminal flag tuples. 
-		# You will feed this to your model to train.
 		return random.sample(self.buffer, batch_size)
 
 	def append(self, transition):
@@ -218,17 +227,17 @@ class DQN_Agent():
 	def __init__(self, environment_name, render=False):
 
 		# Create an instance of the network itself, as well as the memory. 
-		# Here is also a good place to set environmental parameters,
-		# as well as training parameters - number of episodes / iterations, etc.
+
 		# self.q_network = QNetwork(environment_name, dueling = True, model = "./checkpoints/SpaceInvaders-v0-0")
 		self.environment_name = environment_name
 		self.q_network = QNetwork(environment_name)
 		self.replay_memory = Replay_Memory()
 		self.env = gym.make(environment_name)
-		# self.env = gym.wrappers.Monitor(self.env, '.', force = True)
 		self.action_dim = self.env.action_space.n
-		self.epsilon = INIT_EPSILON # this value should be decayed
-		self.gamma = 1
+		self.epsilon = INIT_EPSILON
+		self.gamma = 0.99
+
+		# max episode to train
 		self.episode = 1000000
 
 	def epsilon_greedy_policy(self, q_values, epsilon):
@@ -244,19 +253,21 @@ class DQN_Agent():
 
 	def train(self):
 		# In this function, we will train our network. 
-		# If training without experience replay_memory, then you will interact with the environment 
-		# in this function, while also updating your network parameters. 
 
-		# If you are using a replay memory, you should interact with environment here, and store these 
-		# transitions to memory, while also updating your model.
+		# Evaluate the performance of the model every update_period updates to the network
 		update_count = 0
+		update_period = 5000
+
 		test_count = 0
+
+		# update the max reward to save the checkpoints with best performance
 		max_reward = -10000
+
 		for i_episode in range(self.episode):
 			state = self.env.reset()
-			reward_sum = 0
 
 			if ENVIRONMENT_NAME == 'SpaceInvaders-v0' and MODEL == 'CNN':
+				# stack four frames as a state
 				frame_batch = deque()
 				for f in range(CONSECUTIVE_FRAMES):
 					frame_batch.append(state)
@@ -267,17 +278,19 @@ class DQN_Agent():
 			for t in range(MAX_ITERATION_PER_EPISODE):
 				# self.env.render()
 				action = self.epsilon_greedy_policy(q_values, self.epsilon)
+
+				# Decay the epsilon
 				if (self.epsilon > FINAL_EPSILON):
 					self.epsilon -= (INIT_EPSILON - FINAL_EPSILON) / DECAY_ITERATION
 
 				next_state, reward, done, info = self.env.step(action)
 
-				reward_sum += reward
-
+				# Transform action as a hot key
 				action_input = npy.zeros(self.action_dim)
 				action_input[action] = 1
 
 				if ENVIRONMENT_NAME == 'SpaceInvaders-v0' and MODEL == 'CNN':
+					# Using frame batch rather than single frame
 					next_frame_batch = frame_batch
 					next_frame_batch.append(next_state)
 					next_frame_batch.popleft()
@@ -286,10 +299,15 @@ class DQN_Agent():
 					next_state_q_values = self.q_network.get_q_values([next_state])[0]
 
 				target = reward
+				# If it's not terminal state, calculate the target. 
+				# (next_state[0] <= 0.5 is a trick to avoid the MountainCar-v0 performing wierdly)
 				if (not done) or (ENVIRONMENT_NAME == "MountainCar-v0" and next_state[0] <= 0.5):
 					target += self.gamma * next_state_q_values[self.greedy_policy(next_state_q_values)]
 
 
+				# Append transition to replay memory
+				# Note that I already calculate the target here so I don't need to feed with 
+				# the next state and reward.
 				if ENVIRONMENT_NAME == 'SpaceInvaders-v0' and MODEL == 'CNN':
 					self.replay_memory.append([frame_batch, action_input, target])
 					frame_batch = next_frame_batch
@@ -297,19 +315,24 @@ class DQN_Agent():
 					self.replay_memory.append([state, action_input, target])
 
 				if len(self.replay_memory.buffer) >= self.replay_memory.burn_in:
+					# replay memory is enough to train the model
+
+					# Check the current performance every update_period
 					if update_count == 0:
 						print("episode: ", i_episode, "test: ", test_count)
 						test_reward = self.test()
 						if test_reward > max_reward:
 							print("save")
 							max_reward = test_reward
-							self.q_network.save_model_weights("./checkpoints/MountainCar-v0", test_count)
+							self.q_network.save_model("./checkpoints/CartPole-v0", test_count)
 						test_count += 1
 
 					update_count += 1
-					if update_count == 5000:
+					if update_count == update_period:
 						update_count = 0
-					# train
+
+
+					# Train
 					batch = self.replay_memory.sample_batch()
 					state_batch = []
 					action_batch = []
@@ -329,23 +352,17 @@ class DQN_Agent():
 				if done:
 					break
 
-			# if reward_sum > -200:
-			# 	print("episode", i_episode, " reward: ", reward_sum)
-
-			# if update_count > 1000:
-			# 	update_count = 0
-			# 	print("episode: ", i_episode)
-			# 	self.test()
-			# 	self.q_network.save_model_weights("./checkpoints/SpaceInvaders-v0", i_episode)
-
 
 	def test(self, model_file=None):
-		# Evaluate the performance of your agent over 100 episodes, by calculating cummulative rewards for the 100 episodes.
-		# Here you need to interact with the environment, irrespective of whether you are using a memory. 
+		# Evaluate the performance of your agent over numbers of episodes, by calculating cummulative rewards for all episodes.
+
 		episode_num = 20
 		total_reward = 0
 
 		env = gym.make(self.environment_name)
+
+		# Record the test
+		# self.env = gym.wrappers.Monitor(self.env, '.', force = True)
 
 		for i in range(episode_num):
 			state = env.reset()
@@ -379,33 +396,7 @@ class DQN_Agent():
 		env.close()
 		return ave_reward
 
-	def burn_in_memory():
-		# Initialize your replay memory with a burn_in number of episodes / transitions. 
-
-		pass
-
-def parse_arguments():
-	parser = argparse.ArgumentParser(description='Deep Q Network Argument Parser')
-	parser.add_argument('--env',dest='env',type=str)
-	parser.add_argument('--render',dest='render',type=int,default=0)
-	parser.add_argument('--train',dest='train',type=int,default=1)
-	parser.add_argument('--model',dest='model_file',type=str)
-	return parser.parse_args()
-
 def main(args):
-
-	args = parse_arguments()
-	environment_name = args.env
-
-	# Setting the session to allow growth, so it doesn't allocate all GPU memory. 
-	gpu_ops = tf.GPUOptions(allow_growth=True)
-	config = tf.ConfigProto(gpu_options=gpu_ops)
-	sess = tf.Session(config=config)
-
-	# Setting this as the default tensorflow session. 
-	# keras.backend.tensorflow_backend.set_session(sess)
-
-	# You want to create an instance of the DQN_Agent class here, and then train / test it. 
 
 	environment_name = ENVIRONMENT_NAME
 	agent = DQN_Agent(environment_name)
